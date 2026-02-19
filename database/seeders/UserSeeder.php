6a834f0008a357f1a6336a09364971ea7cb0b384
Christian Ponis <cripo75@gmail.com>
2026-02-19 20:46:47 +0100

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -9,54 +9,26 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'name' => 'Admin Porty',
-            'email' => 'admin@porty.it',
-            'password' => bcrypt('password'),
-            'role' => 'admin',
-            'phone' => '+39 333 0000001',
-            'is_active' => true,
-            'email_verified_at' => now(),
-        ]);
+        $users = [
+            ['email' => 'admin@porty.it',  'name' => 'Admin Porty',   'role' => 'admin',  'phone' => '+39 333 0000001'],
+            ['email' => 'owner@porty.it',  'name' => 'Marco Rossi',   'role' => 'owner',  'phone' => '+39 333 0000002'],
+            ['email' => 'owner2@porty.it', 'name' => 'Giulia Bianchi','role' => 'owner',  'phone' => '+39 333 0000003'],
+            ['email' => 'guest@porty.it',  'name' => 'Luca Verdi',    'role' => 'guest',  'phone' => '+39 333 0000004'],
+            ['email' => 'guest2@porty.it', 'name' => 'Anna Marino',   'role' => 'guest',  'phone' => '+39 333 0000005'],
+        ];
 
-        User::create([
-            'name' => 'Marco Rossi',
-            'email' => 'owner@porty.it',
-            'password' => bcrypt('password'),
-            'role' => 'owner',
-            'phone' => '+39 333 0000002',
-            'is_active' => true,
-            'email_verified_at' => now(),
-        ]);
-
-        User::create([
-            'name' => 'Giulia Bianchi',
-            'email' => 'owner2@porty.it',
-            'password' => bcrypt('password'),
-            'role' => 'owner',
-            'phone' => '+39 333 0000003',
-            'is_active' => true,
-            'email_verified_at' => now(),
-        ]);
-
-        User::create([
-            'name' => 'Luca Verdi',
-            'email' => 'guest@porty.it',
-            'password' => bcrypt('password'),
-            'role' => 'guest',
-            'phone' => '+39 333 0000004',
-            'is_active' => true,
-            'email_verified_at' => now(),
-        ]);
-
-        User::create([
-            'name' => 'Anna Marino',
-            'email' => 'guest2@porty.it',
-            'password' => bcrypt('password'),
-            'role' => 'guest',
-            'phone' => '+39 333 0000005',
-            'is_active' => true,
-            'email_verified_at' => now(),
-        ]);
+        foreach ($users as $data) {
+            User::firstOrCreate(
+                ['email' => $data['email']],
+                [
+                    'name'              => $data['name'],
+                    'password'          => bcrypt('password'),
+                    'role'              => $data['role'],
+                    'phone'             => $data['phone'],
+                    'is_active'         => true,
+                    'email_verified_at' => now(),
+                ]
+            );
+        }
     }
 }
